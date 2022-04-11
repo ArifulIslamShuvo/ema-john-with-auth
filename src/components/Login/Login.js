c97e407e83a9565d9cf0683ddd8c08e34443eb63
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-12 04:30:24 +0600

    setup signup component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Login.css'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
     return (
@@ -10,18 +10,29 @@ const Login = () => {
                     <h2 className='form-title'>This is Login </h2>
                     <div className='input-group'>
                         <label htmlFor="Email">Email</label>
-                        <input type="email" name="email" id="" />
+                        <input type="email" name="email" id="" required/>
                     </div>
                     <div className='input-group'>
                         <label htmlFor="password">Password</label>
-                        <input type="password" name="password" id="" />
+                        <input type="password" name="password" id="" required/>
                     </div>
-                    <input className='form-submit' type="submit" value="Login" />
+                    <input className='form-submit' type="submit" value="Login" required/>
                 </div>
                 <p>
                     New to Ema-Joha? <Link className='form-link' to="/signup">Create an account</Link>
                 </p>
+                <div className='horizontal-line'>
+
+                    <hr className="horiz" />
+                    or
+                    <hr className="horiz" />
+                </div>
+                <button className='form-google'>
+                    <img className='google-img' src="https://logowik.com/content/uploads/images/985_google_g_icon.jpg" alt="" />
+                   <p> Continue with Google</p>
+                </button>
             </form>
+
         </div>
     );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,11 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './SignUp.css'
 
 const SignUp = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+    
+    const handleEmailBlur = event =>{
+        setEmail(event.target.value);
+    }
+    const handlePasswordBlur = event =>{
+        setPassword(event.target.value);
+    }
+    const handleConfirmPasswordBlur = event =>{
+        setConfirmPassword(event.target.value);
+    }
+
+    const handleCreateUser = event => {
+        event.preventDefult();
+        if(password !== confirmPassword){
+            setError('Your passwords did not match');
+        }
+    }
+
+
     return (
-        <div>
-            <h2>This is SignUp</h2>
-        </div>
+        <div className='form-container'>
+        <form onSubmit={handleCreateUser}>
+            <div>
+                <h2 className='form-title'>Sign Up </h2>
+                <div className='input-group'>
+                    <label htmlFor="Email">Email</label>
+                    <input onBlur={handleEmailBlur} type="email" name="email" id=""  required/>
+                </div>
+                <div className='input-group'>
+                    <label htmlFor="password">Password</label>
+                    <input onBlur={handlePasswordBlur} type="password" name="password" id="" required/>
+                </div>
+                <div className='input-group'>
+                    <label htmlFor="confirm-password">Confirm Password</label>
+                    <input onBlur={handleConfirmPasswordBlur} type="password" name="confirm-password" id="" required/>
+                </div>
+
+                <p style={{color:'red'}}>{error}</p>
+
+                <input className='form-submit' type="submit" value="Sign up" />
+            </div>
+            <p>
+                Already have an account? <Link className='form-link' to="/login">Login</Link>
+            </p>
+            <div className='horizontal-line'>
+
+                <hr className="horiz" />
+                or
+                <hr className="horiz" />
+            </div>
+            <button className='form-google'>
+                <img className='google-img' src="https://logowik.com/content/uploads/images/985_google_g_icon.jpg" alt="" />
+               <p> Continue with Google</p>
+            </button>
+        </form>
+
+    </div>
     );
 };
 
