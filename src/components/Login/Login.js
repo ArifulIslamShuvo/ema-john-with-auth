0850c771e11b39ce7c6826537296e8da53264f4b
Ariful Islam Shuvo <arifulislams063@gmail.com>
2022-04-13 00:54:50 +0600

    implement Require auth for auth redirect

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import './Login.css';
 import auth from '../../firebase.init';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 
 const Login = () => {
@@ -16,6 +16,8 @@ const Login = () => {
         error,
       ] = useSignInWithEmailAndPassword(auth);
       const navigate = useNavigate();
+      let location = useLocation();
+      const from = location.state?.from?.pathname || "/";
 
     const handleEmailBlur = event => {
         setEmail(event.target.value);
@@ -25,7 +27,7 @@ const Login = () => {
         setPassword(event.target.value);
     }
     if(user){
-        navigate('/shop');
+        navigate(from, { replace: true });
     }
 
     const handleUserSignIn = event => {
